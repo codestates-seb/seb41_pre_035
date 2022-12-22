b78d9f8c3fae2b64c0dcadec74d4d9207a06fc14
cjswo4034 <cjswo4034@gmail.com>
2022-12-22 12:27:34 +0900

    feat: Question 엔티티 구현
    - 모든 연관관계는 추후 도메인 엔티티 구현시 매핑예정
    - BaseEntity
      - 테스트를 위해 기본값으로 초기화
      - ddl-auto 기능을 사용하므로 default값을 주기 위해서 @ColumnDefault 어노테이션 추가

--- server/src/main/java/com/codestates/sof/domain/common/BaseEntity.java
+++ server/src/main/java/com/codestates/sof/domain/common/BaseEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,10 +18,12 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 	@CreatedDate
+	@ColumnDefault("CURRENT_TIMESTAMP()")
 	@Column(name = "created_at", nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+	private LocalDateTime createdAt = LocalDateTime.now();
 
 	@LastModifiedDate
+	@ColumnDefault("CURRENT_TIMESTAMP()")
 	@Column(name = "last_modified_at", nullable = false)
-	private LocalDateTime lastModifiedAt;
+	private LocalDateTime lastModifiedAt = LocalDateTime.now();
 }
