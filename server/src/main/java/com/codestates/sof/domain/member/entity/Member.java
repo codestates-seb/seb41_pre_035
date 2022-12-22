2f9da56f293eaeb03b67cfb87b02e926b4ce6d6e
my-roki <anthonychangrok@gmail.com>
2022-12-22 14:02:39 +0900

    [#9] feat: Member entity 구현

---
+++ server/src/main/java/com/codestates/sof/domain/member/entity/Member.java
@@ -0,0 +1,46 @@
+package com.codestates.sof.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.codestates.sof.domain.common.BaseEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "MEMBERS")
+public class Member extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long memberId;
+
+	@Column(nullable = false, updatable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false, length = 127)
+	private String name;
+
+	@Column(nullable = false, length = 63)
+	private String encryptedPassword;
+
+	@Column(length = 63)
+	private String beforeEncryptedPassword;
+
+	@Column(nullable = false)
+	private boolean verificationFlag = false;
+
+	@Column(nullable = false)
+	private boolean deleteFlag = false;
+
+	@Column(nullable = false)
+	private LocalDateTime lastActivateAt = LocalDateTime.now();
+}
