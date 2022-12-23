689dfa926f9fb43138a0261204819fa89dd5d8ab
dev-sena <leesena948@gmail.com>
2022-12-23 21:06:03 +0900

    auth: 로그인과 관련된 페이지, 회원가입 페이지 화면 구현

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,29 @@ import Header from "./Components/Header";
 import HeaderLogin from "./Components/HeaderLogin";
 import HeaderMenumodal from "./Components/HeaderMenumodal";
 
+// ! 위 코드 수정해주세요
+import Login from "./page/Login";
+import Recovery from "./page/Recovery";
+import SignUp from "./page/SignUp";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">
-      <Header/>
+  return (
+    <div className="App">
+      <Header />
       {/* <HeaderLogin/> */}
       {/* <HeaderMenumodal/> */}
-      <Nav/>
-      <Sidebar/>
-      <Footer/>
-  </div>;
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+
+      {/* <Nav />
+      <Sidebar />
+      <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className='sidebarContainer'>
-        <div className='blogFeature'>
-            <div className='blog'>
-                <div className='blogFeatureTitle'>The Overflow Blog</div>
-                <div className='blogFeatureContents'>contents</div>
-            </div>
-            <div className='feature'>
-                <div className='blogFeatureTitle'>Featured on Meta</div>
-                <div className='blogFeatureContents'>contents</div>
-            </div>
+    <div className="sidebarContainer">
+      <div className="blogFeature">
+        <div className="blog">
+          <div className="blogFeatureTitle">The Overflow Blog</div>
+          <div className="blogFeatureContents">contents</div>
         </div>
-        <div className='customFilters'>
-                <div className='filtertagTitle'>Custom Filters</div>
-                <div className='fiterContents'>Create a custom filter</div>
+        <div className="feature">
+          <div className="blogFeatureTitle">Featured on Meta</div>
+          <div className="blogFeatureContents">contents</div>
         </div>
-        <div className='watchedTags'>
-                <div className='filtertagTitle'>Watched Tags</div>
-                <div className='tagContents'>Watch tags to curate your list of questions.</div>
-        </div>
+      </div>
+      <div className="customFilters">
+        <div className="filtertagTitle">Custom Filters</div>
+        <div className="fiterContents">Create a custom filter</div>
+      </div>
+      <div className="watchedTags">
+        <div className="filtertagTitle">Watched Tags</div>
+        <div className="tagContents">Watch tags to curate your list of questions.</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
