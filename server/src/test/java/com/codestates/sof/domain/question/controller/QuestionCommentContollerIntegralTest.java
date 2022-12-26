0a01abe1bf79ee3923b95bb04e51687c2500fc65
cjswo4034 <cjswo4034@gmail.com>
2022-12-26 17:05:19 +0900

    [#47] test: 댓글 수정 기능에 대한 테스트 케이스 작성

--- server/src/test/java/com/codestates/sof/domain/question/controller/QuestionCommentContollerIntegralTest.java
+++ server/src/test/java/com/codestates/sof/domain/question/controller/QuestionCommentContollerIntegralTest.java
@@ -4,9 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
 
+import javax.persistence.EntityManager;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -21,11 +31,13 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.codestates.sof.domain.member.entity.Member;
+import com.codestates.sof.domain.member.repository.MemberRepository;
 import com.codestates.sof.domain.member.service.MemberService;
 import com.codestates.sof.domain.question.dto.QuestionCommentDto;
 import com.codestates.sof.domain.question.entity.Question;
 import com.codestates.sof.domain.question.entity.QuestionComment;
 import com.codestates.sof.domain.question.repository.QuestionCommentRepository;
+import com.codestates.sof.domain.question.repository.QuestionRepository;
 import com.codestates.sof.domain.question.service.QuestionCommentService;
 import com.codestates.sof.domain.question.service.QuestionService;
 import com.codestates.sof.domain.stub.QuestionCommentStub;
@@ -37,6 +49,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @ActiveProfiles("local")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("질문 댓글 기능 테스트")
 public class QuestionCommentContollerIntegralTest {
 	@Autowired
 	MockMvc mvc;
@@ -60,10 +73,16 @@ public class QuestionCommentContollerIntegralTest {
 	QuestionCommentService commentService;
 
 	@Autowired
-	QuestionCommentRepository repository;
+	MemberRepository memberRepository;
+
+	@Autowired
+	QuestionRepository questionRepository;
+
+	@Autowired
+	QuestionCommentRepository commentRepository;
 
 	@Nested
-	@DisplayName("Tests for the post method")
+	@DisplayName("댓글 등록")
 	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 	class Post {
 		Question question;
@@ -158,5 +177,121 @@ public class QuestionCommentContollerIntegralTest {
 		void 회원이_아니면_댓글을_등록할_수_없다() throws Exception {
 			// TODO (Auth)
 		}
+
+		@AfterAll
+		void clear() {
+			clearAll();
+		}
+	}
+
+	@Nested
+	@DisplayName("댓글 수정")
+	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+	class Patch {
+
+		TriFunction<Long, Long, Object, ResultActions> patchPerform = (questionId, commentId, content) ->
+			mvc.perform(
+				patch("/questions/{question-id}/comments/{comment-id}", questionId, commentId)
+					.accept(MediaType.APPLICATION_JSON)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(om.writeValueAsString(content))
+			);
+		QuestionComment comment;
+
+		Question question;
+		Member member;
+		@BeforeAll
+		void setUp() {
+			Member member = new Member();
+			member.setName("name");
+			member.setEmail("email");
+			member.setName("name");
+			member.setEncryptedPassword("encryptedPassword");
+
+			this.member = memberService.createMember(member);
+			this.question = questionService.write(new Question(member, "title", "content"));
+		}
+
+		@AfterAll
+		void clear() {
+			clearAll();
+		}
+
+		@BeforeEach
+		void beforeEach() {
+			comment = commentService.comment(question.getQuestionId(),
+				new QuestionComment(member, question, "content"));
+		}
+
+		@AfterEach
+		void afterEach() {
+			commentRepository.deleteAll();
+		}
+
+		@Test
+		void 댓글을_수정하면_수정한_시간이_변경된다() throws Exception {
+			// given
+			QuestionCommentDto.Patch patch = QuestionCommentStub.getDefaultPatch();
+			LocalDateTime modifiedAtBeforePatch = comment.getLastModifiedAt();
+
+			// when
+			ResultActions actions = patchPerform.apply(question.getQuestionId(), comment.getQuestionCommentId(), patch);
+
+			// given
+			actions
+				.andExpect(status().isOk());
+
+			Optional<QuestionComment> optComment = commentRepository.findById(comment.getQuestionCommentId());
+			assertTrue(optComment.isPresent());
+			assertTrue(optComment.get().getLastModifiedAt().isAfter(modifiedAtBeforePatch));
+			assertEquals(patch.getContent(), optComment.get().getContent());
+		}
+
+		@Test
+		void 댓글내용이_없으면_실패한다() throws Exception {
+			// given
+			QuestionCommentDto.Patch patch = QuestionCommentStub.getDefaultPatch();
+			patch.setContent("");
+
+			Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+			Set<ConstraintViolation<QuestionCommentDto.Patch>> validate = validator.validate(patch);
+
+			// when
+			ResultActions actions = patchPerform.apply(question.getQuestionId(), comment.getQuestionCommentId(), patch);
+
+			// given
+			actions
+				.andExpect(status().isBadRequest());
+
+			assertTrue(validate.iterator().hasNext());
+			assertEquals("content", validate.iterator().next().getPropertyPath().toString());
+		}
+
+		@Test
+		void 작성자가_아니면_실패한다() throws Exception {
+			// given
+			QuestionCommentDto.Patch patch = QuestionCommentStub.getDefaultPatch();
+			patch.setModifierId((long)Integer.MAX_VALUE);
+
+			// when
+			ResultActions actions = patchPerform.apply(question.getQuestionId(), comment.getQuestionCommentId(), patch);
+
+			// given
+			actions
+				.andExpect(status().isForbidden());
+		}
+
+	}
+
+	private void clearAll() {
+		commentRepository.deleteAll();
+		questionRepository.deleteAll();
+		memberRepository.deleteAll();
+	}
+
+	@FunctionalInterface
+	interface TriFunction<A, B, C, D> {
+
+		D apply(A a, B b, C c) throws Exception;
 	}
 }
