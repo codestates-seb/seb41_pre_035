fe9bf0426b3b9bcfd2457fe5c685fc3527fe7a06
mmyona <hyon2001@ajou.ac.kr>
2022-12-28 01:46:02 +0900

    [Question] 개별 질문 페이지 팝업창 구현

--- client/src/page/QuestionPage.js
+++ client/src/page/QuestionPage.js
@@ -6,15 +6,37 @@ import "../css/Btn.css";
 import "../css/QuestionPage.css";
 import questionList from "../data/Questions";
 import MDEditor from "@uiw/react-md-editor";
+const axios = require("axios");
 
 function QuestionPage() {
   const { questionId } = useParams();
   const navigate = useNavigate();
   const [answer, setAnswer] = useState("");
+  const [pop, setPop] = useState(true);
 
   const question = questionList.filter((el) => el.id === Number(questionId))[0];
   const handleClick = () => {
     navigate("/questions");
+  };
+  const AnswerBody = JSON.stringify({
+    memberId: 0,
+    title: "title",
+    body: answer,
+    tags: [],
+  });
+
+  const handleAnswerSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://34.64.179.131:8080/answers", AnswerBody, {
+        "Accept-Language": "ko",
+        "Content-Type": "application/json",
+      })
+      .then((res) => {
+        if (res.ok) {
+          alert("생성이 완료 되었습니다.");
+        }
+      });
   };
   return (
     <div className="sqContent">
@@ -54,8 +76,23 @@ function QuestionPage() {
       <div className="yourAnswer">
         <h2>Your Answer</h2>
         <MDEditor value={answer} onChange={setAnswer} />
+        {pop === true && answer && (
+          <div className="qPopup">
+            <div className="popupText">
+              <p>Thanks for contributing an answer to Stack Overflow!</p>
+              <li>Please be sure to answer the question. Provide details and share your research!</li>
+              <p>But avoid …</p>
+              <li>Asking for help, clarification, or responding to other answers.</li>
+              <li>Making statements based on opinion; back them up with references or personal experience.</li>
+              <p>To learn more, see our tips on writing great answers.</p>
+            </div>
+            <button id="popupClose" onClick={() => setPop(false)}>
+              x
+            </button>
+          </div>
+        )}
         <MDEditor.Markdown source={answer} style={{ whiteSpace: "pre-wrap" }} />
-        <button className="btn" onClick={handleClick}>
+        <button className="btn" onClick={handleAnswerSubmit}>
           Post Your Answer
         </button>
       </div>
