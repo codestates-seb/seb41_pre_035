65cc7367ab19cad5e80448f30d6f255f6a51a331
my-roki <anthonychangrok@gmail.com>
2022-12-27 22:12:17 +0900

    [#45] feat: 로그인 적용 SecurityConfig 작성

--- server/src/main/java/com/codestates/sof/global/config/SecurityConfig.java
+++ server/src/main/java/com/codestates/sof/global/config/SecurityConfig.java
@@ -7,7 +7,9 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +17,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.codestates.sof.domain.auth.filter.JwtAuthenticationFilter;
+import com.codestates.sof.domain.auth.handler.MemberAuthenticationFailureHandler;
+import com.codestates.sof.domain.auth.handler.MemberAuthenticationSuccessHandler;
+import com.codestates.sof.domain.auth.jwt.JwtTokenizer;
+import com.codestates.sof.domain.auth.mapper.LoginMapper;
+
+import lombok.AllArgsConstructor;
+
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
+	private final JwtTokenizer jwtTokenizer;
+	private final LoginMapper mapper;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		// TODO : 필요한 Security 설정을 더 구체화 해야합니다.
@@ -26,6 +40,8 @@ public class SecurityConfig {
 			.cors(withDefaults())
 			.formLogin().disable()
 			.httpBasic().disable()
+			.apply(new CustomFilterConfigure())
+			.and()
 			.authorizeHttpRequests(a -> a
 				.antMatchers("/auth/**", "/h2/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/members").permitAll()
@@ -50,5 +66,21 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	private class CustomFilterConfigure extends AbstractHttpConfigurer<CustomFilterConfigure, HttpSecurity> {
+		@Override
+		public void configure(HttpSecurity httpSecurity) {
+			AuthenticationManager authenticationManager = httpSecurity.getSharedObject(AuthenticationManager.class);
+
+			JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager,
+				jwtTokenizer);
+
+			jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+			jwtAuthenticationFilter.setAuthenticationSuccessHandler(
+				new MemberAuthenticationSuccessHandler(mapper));
+			jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
+			httpSecurity.addFilter(jwtAuthenticationFilter);
+		}
 	}
 }
