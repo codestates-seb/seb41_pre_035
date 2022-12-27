e235428cfebfe4a6e173043d7f67e445a17e4e50
my-roki <anthonychangrok@gmail.com>
2022-12-27 11:56:54 +0900

    [#53] refact: EmailSendable 구현체에 대한 Profile 적용.

--- server/src/main/java/com/codestates/sof/global/email/MockEmailSendable.java
+++ server/src/main/java/com/codestates/sof/global/email/MockEmailSendable.java
@@ -2,15 +2,30 @@ package com.codestates.sof.global.email;
 
 import java.util.Map;
 
+import com.codestates.sof.domain.member.entity.Member;
+import com.codestates.sof.domain.member.repository.MemberRepository;
+import com.codestates.sof.global.error.dto.ExceptionCode;
+import com.codestates.sof.global.error.exception.BusinessLogicException;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@AllArgsConstructor
 public class MockEmailSendable implements EmailSendable {
+	private final MemberRepository memberRepository;
 
 	@Override
-	public void send(String[] to, String subject,  Map<String, Object> variables, String templateName) {
+	public void send(String[] to, String subject, Map<String, Object> variables, String templateName) {
 		for (String t : to) {
-			log.info("# Send a email to : " + t);
+			log.info("# Send a mock email to : " + t);
+
+			// 이메일 인증을 했다고 가정합니다.
+			Member member = memberRepository.findByEmail(t).orElseThrow(() ->
+				new BusinessLogicException(ExceptionCode.NOT_FOUND_MEMBER)
+			);
+			member.setVerificationFlag(true);
+			memberRepository.save(member);
 		}
 	}
 }
