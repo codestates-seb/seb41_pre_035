fd4617a19887907fc23447bf2c361b96656992ed
cjswo4034 <cjswo4034@gmail.com>
2022-12-27 18:48:56 +0900

    [#57] feat: 질문 및 댓글 조회에 대한 핸들러 메서드 구현

--- server/src/main/java/com/codestates/sof/domain/question/controller/QuestionCommentContoller.java
+++ server/src/main/java/com/codestates/sof/domain/question/controller/QuestionCommentContoller.java
@@ -1,22 +1,29 @@
 package com.codestates.sof.domain.question.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codestates.sof.domain.question.dto.QuestionCommentDto;
+import com.codestates.sof.domain.question.dto.QuestionCommentResponseDto;
 import com.codestates.sof.domain.question.entity.QuestionComment;
 import com.codestates.sof.domain.question.mapper.QuestionCommentMapper;
 import com.codestates.sof.domain.question.service.QuestionCommentService;
+import com.codestates.sof.global.dto.MultiResponseDto;
 import com.codestates.sof.global.dto.SingleResponseDto;
 
 import lombok.RequiredArgsConstructor;
@@ -31,18 +38,18 @@ public class QuestionCommentContoller {
 	private final QuestionCommentMapper mapper;
 
 	@PostMapping
-	public ResponseEntity<SingleResponseDto<QuestionCommentDto.Response>> post(
+	public ResponseEntity<SingleResponseDto<QuestionCommentResponseDto.Response>> post(
 		@PathVariable("question-id") @Positive long questionId,
 		@RequestBody @Valid QuestionCommentDto.Post post) {
 
 		QuestionComment qc = commentService.comment(questionId, mapper.postToQuestionComment(post));
-		QuestionCommentDto.Response response = mapper.questionCommentToResponse(qc);
+		QuestionCommentResponseDto.Response response = mapper.commentToResponse(qc);
 
 		return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
 	}
 
 	@PatchMapping("/{comment-id}")
-	public ResponseEntity<SingleResponseDto<QuestionCommentDto.Response>> patch(
+	public ResponseEntity<SingleResponseDto<QuestionCommentResponseDto.Response>> patch(
 		@PathVariable("question-id") @Positive long questionId,
 		@PathVariable("comment-id") @Positive long commentId,
 		@RequestBody @Valid QuestionCommentDto.Patch patch
@@ -51,9 +58,20 @@ public class QuestionCommentContoller {
 		patchToComment.setQuestionCommentId(commentId);
 
 		QuestionComment comment = commentService.modify(questionId, patch.getModifierId(), patchToComment);
-		QuestionCommentDto.Response response = mapper.questionCommentToResponse(comment);
+		QuestionCommentResponseDto.Response response = mapper.commentToResponse(comment);
 
 		return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+	}
+
+	@GetMapping
+	public ResponseEntity<MultiResponseDto<QuestionCommentResponseDto.SimpleResponse>> getAll(
+		@PathVariable("question-id") @Positive long questionId,
+		@RequestParam(value = "page", defaultValue = "1") int page,
+		@RequestParam(value = "size", defaultValue = "5") int size
+	) {
+		Page<QuestionComment> comments = commentService.findAll(questionId, page - 1, size);
+		List<QuestionCommentResponseDto.SimpleResponse> responses = comments.map(mapper::commentToSimpleResponse).toList();
+		return new ResponseEntity<>(new MultiResponseDto<>(responses, comments), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{comment-id}")
