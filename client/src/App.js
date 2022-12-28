7058d5eba0b1bfc953988b7f242084c49da232ca
임현아 <90781929+mmyona@users.noreply.github.com>
2022-12-28 15:06:27 +0900

    Update App.js
    SignUp -> Signup 으로 경로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import HeaderLogin from "./component/HeaderLogin";
 import HeaderMenumodal from "./component/HeaderMenumodal";
 import Login from "./page/Login";
 import Recovery from "./page/Recovery";
-import Signup from "./page/SignUp";
+import Signup from "./page/Signup";
 import SignupSuccess from "./page/SignupSuccess";
 
 import { useEffect, useState } from "react";
