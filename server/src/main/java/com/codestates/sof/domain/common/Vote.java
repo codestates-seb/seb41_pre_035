6b9e124f4c325a7105a375a822ba8f17d1ab84d4
cjswo4034 <cjswo4034@gmail.com>
2022-12-29 13:10:32 +0900

    [#83] feat: Tag Entity 구현

---
+++ server/src/main/java/com/codestates/sof/domain/common/Vote.java
@@ -0,0 +1,35 @@
+package com.codestates.sof.domain.common;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+
+import com.codestates.sof.domain.member.entity.Member;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// Inheritance 사용안한이유: https://ict-nroo.tistory.com/128
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class Vote extends BaseEntity {
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false, updatable = false)
+	private Member member;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "vote_type", nullable = false)
+	private VoteType type = VoteType.NONE;
+
+	public Vote(Member member, VoteType type) {
+		this.member = member;
+		this.type = type;
+	}
+}
