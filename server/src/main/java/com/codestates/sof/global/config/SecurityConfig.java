64f0d4aa17dc1cc8d78cecd8109c1476bd8e68ac
cryoon <anthonychangrok@gmail.com>
2022-12-30 14:02:43 +0900

    CORS 설정 변경 (#107)
    * refact : cors 설정 변경 및 AuthController 응답값 변경
    
    * refact : 테스트 코드 변경

--- server/src/main/java/com/codestates/sof/global/config/SecurityConfig.java
+++ server/src/main/java/com/codestates/sof/global/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.codestates.sof.global.config;
 
-import static org.springframework.security.config.Customizer.*;
-
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -44,7 +42,8 @@ public class SecurityConfig {
 		httpSecurity.headers().frameOptions().sameOrigin()
 			.and()
 			.csrf().disable()
-			.cors(withDefaults())
+			.cors()
+			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.formLogin().disable()
@@ -74,8 +73,9 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 
 		// TODO : CORS 도메인 설정이 필요합니다.
-		configuration.setAllowedOrigins(List.of("*"));
-		configuration.setAllowedMethods(List.of("POST", "GET", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:3000"));
+		configuration.setAllowedMethods(List.of("POST", "GET", "PATCH", "DELETE", "OPTIONS", "HEAD"));
+		configuration.setAllowedHeaders(List.of("*"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
